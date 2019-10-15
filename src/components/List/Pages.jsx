9486fc6991ef99f26de6cbcbab171ef1a5f0032a
Ed React <edreactdeveloper@gmail.com>
2019-10-15 10:25:37 +0300

    search bar styles

--- src/components/List/Pages.jsx
+++ src/components/List/Pages.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import style from './List.module.scss';
-import {Pagination} from 'react-bootstrap';
+import { Pagination } from 'react-bootstrap';
 
 const Pages = ({ pages, goToPageAction, currentPage }) => {
   const array = []
   for (let i = 1; i <= pages; i++) {
     array.push(i)
   }
-  return <div className={style.pagination_wrapper }>  
-  <Pagination>
-    {array.map(number => <Pagination.Item
-      key={number}
-      onClick={() => goToPageAction(number)}
-      active={currentPage === number}
-  
+  return <div className={style.pagination_wrapper}>
+    <Pagination>
+      {array.map(number => <Pagination.Item
+        key={number}
+        onClick={() => goToPageAction(number)}
+        active={currentPage === number}
       >
-      {number}
-    </Pagination.Item>)}
-  </Pagination>
-      </div>
+        {number}
+      </Pagination.Item>)}
+    </Pagination>
+  </div>
 };
 
 export default Pages;
--- src/components/List/Search/index.jsx
+++ src/components/List/Search/index.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Sorting from './Sorting';
+import {FormControl, Button} from 'react-bootstrap';
+import style from './Search.module.scss'; 
 
 const Seacrch = ({search, submitSearch, fetchSearchAction, sortAction}) => {
   return (
-    <form onSubmit={submitSearch}>
-      <input type="text" name="search" value={search} onChange={(e)=> fetchSearchAction(e.target.value)} />
-      <button type="submit">search</button>
+    <form onSubmit={submitSearch} className={style.search} >
+      <div className={style.search_wrapper}>
+      <FormControl 
+      type="text" 
+      name="search" 
+      value={search} 
+      onChange={(e)=> fetchSearchAction(e.target.value)} 
+      className={style.search_bar} 
+      />
+      <Button className={style.search_btn} type="submit">search</Button>
+      </div>
       <Sorting sortAction={sortAction}/>
     </form>
   );
